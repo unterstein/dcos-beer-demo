ead6bb203ffb33597ec11f8d8f0cf13ce12a55cb
Johannes Unterstein <junterstein@mesosphere.io>
2017-10-02 14:07:20 +0200

    initial commit

---
+++ src/main/java/io/dcos/examples/beerdemo/BeerDemoApplication.java
@@ -0,0 +1,12 @@
+package io.dcos.examples.beerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerDemoApplication.class, args);
+	}
+}
