e0958f320f7db35b8ed125031a17ca8d2d1f71d0
Johannes Unterstein <junterstein@mesosphere.io>
2017-10-02 14:28:40 +0200

    formatting

--- src/main/java/io/dcos/examples/beerdemo/BeerDemoApplication.java
+++ src/main/java/io/dcos/examples/beerdemo/BeerDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeerDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeerDemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BeerDemoApplication.class, args);
+  }
 }
